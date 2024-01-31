9945e7dcc8e8364456fccc9b5df40c44b771d90c
Elgzprtma <dikaelang13@gmail.com>
2024-01-31 15:12:58 +0700

    SPRING-JWT_SECURITY

---
+++ src/test/java/com/asep/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asep.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
